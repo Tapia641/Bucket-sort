1cb7ae785ed2fbe5664e40ad4ac9264ba3cec815
Luis Tapia <tapia641@gmail.com>
2019-04-04 00:21:45 -0600

    Separando cubetas finalizado

--- BucketSort.cpp
+++ BucketSort.cpp
@@ -6,8 +6,9 @@
 #include <string>
 #include <vector> //La estructura ideal ;)
 #include <algorithm> //Para ocupar sort con complejidad de O(nlogn)
-#include <cstdlib> //rand
+#include <cstdlib> //rand()
 #include <ctime> //sacar verdaderos aleatorios
+#include <cmath> //Para ceil
 
 using namespace std;
 #define MAX 3500
@@ -26,23 +27,48 @@ struct DATA
 
 
 //0-RangoMax
-void DivideCubetas(vector< vector<int> > &TotalCubetas, int &N){
+void DivideCubetas(vector<vector<int> > &TotalCubetas, int &N){
 
 	//Variable local
 	vector<int> v;
-	int NumeroAleatorio, M = 0;
+	int NumeroAleatorio, M = 0, Division = 0;
 
 	//Generamos los 3500 numeros en 0-999
+	srand(time(NULL));
+
 	for (int i = 0; i < MAX; ++i)
 	{
 		NumeroAleatorio = (rand()%999)+1;
-		//M = max(M,NumeroAleatorio);
-		cout<<NumeroAleatorio<<endl;
+		M = max(M, NumeroAleatorio);
 		v.push_back(NumeroAleatorio);
 	}
 
-	cout<<M<<endl;
+	//Establecemos los rangos de para cada cubeta
+	Division = floor(M/N);
+	vector<int> Rango;
 
+	//Establecemos el limite de cada rango
+	for (int i = 0; i < N; ++i)
+	{
+		Rango.push_back(Division*i);
+	}
+
+	//Segmentamos cada numero en cada cubeta
+	vector<int> CubetaSimple;
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < MAX; ++j)
+		{
+			if (v[j] > Rango[i] && v[j] <= Rango[i+1])
+			{
+				cout<<v[j]<<endl;
+				CubetaSimple.push_back(v[i]);
+			}
+		}
+
+		TotalCubetas.push_back(CubetaSimple);
+		CubetaSimple.clear();
+	}
 }
 
 
@@ -57,6 +83,7 @@ int main(int argc, char const *argv[])
 		//Vector que lleva las cubetas
 		vector< vector<int> > TotalCubetas;
 		DivideCubetas(TotalCubetas, CUBETAS);
+		cout<<"TotalCubetas: "<<TotalCubetas.size()<<endl;
 
 		//Creamos un arreglo de hilos con tamanio de las cubetas
 		pthread_t *TotalHilos = (pthread_t *)malloc(CUBETAS *sizeof(pthread_t));
@@ -65,7 +92,7 @@ int main(int argc, char const *argv[])
 		for (int i = 0; i < CUBETAS; ++i)
 		{
 			//A cada hilo le mandamos cada una de las cubetas
-			//pthread_create(&TotalHilos[i], NULL, Ordena(DivideCubetas[i]), NULL);
+			pthread_create(&TotalHilos[i], NULL, Ordena(DivideCubetas[i]), NULL);
 		}
 
 		//Esperamos a los hilos culminen
